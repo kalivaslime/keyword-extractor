7546a7a3171940112c84dfbb5321119bc16b7961
Christos Kalivas <chrkalivas@gmail.com>
2023-04-05 20:00:55 +0300

    chakra ui, colors theme 💅

---
+++ src/draculaColors.ts
@@ -0,0 +1,13 @@
+export default {
+  darker: '#282a36',
+  dark: '#44475a',
+  light: '#f8f8f2',
+  blue: '#6272a4',
+  cyan: '#8be9fd',
+  green: '#50fa7b',
+  orange: '#ffb86c',
+  pink: '#ff79c6',
+  purple: '#bd93f9',
+  red: '#ff5555',
+  yellow: '#f1fa8c',
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {ChakraProvider} from '@chakra-ui/react'
+import {extendTheme} from '@chakra-ui/react'
+import colors from './draculaColors'
+
+const theme = extendTheme({
+  colors,
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
 )
